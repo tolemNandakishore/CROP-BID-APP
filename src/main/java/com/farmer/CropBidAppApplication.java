c4d42090ce622d9fc03824877d6bc3f06b2c60f0
Nanda Kishore <tnandakishore723@gmail.com>
2024-01-10 22:53:33 +0530

    project started

---
+++ src/main/java/com/farmer/CropBidAppApplication.java
@@ -0,0 +1,13 @@
+package com.farmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropBidAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropBidAppApplication.class, args);
+	}
+
+}
